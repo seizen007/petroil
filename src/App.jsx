cf72618c3ed16d3560c67663e95f40c62deca9d1
Ashraful Islam Siam <ashraful199113@gmail.com>
2023-09-19 14:18:04 +0600

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Navbar  from './components/Navbar/Navbar'
-import Banner from './components/banner/Banner'
+import Banner from '/src/components/banner/Banner'
 
 function App() {
   return (
