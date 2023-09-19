49d19becaaee386cdeacc8646b9eddf3de64fa2b
Ashraful Islam Siam <ashraful199113@gmail.com>
2023-09-19 22:01:42 +0600

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Navbar  from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 
 
+
 function App() {
   return (
     <>
     <Header />
     <Navbar />
-    <Hero />
-
+    <Hero/>
     </>
   )
 }
