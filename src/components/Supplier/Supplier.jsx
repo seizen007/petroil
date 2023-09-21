c82f1735d9b17abdfbd7538faab8e01cc227108d
Ashraful Islam Siam <ashraful199113@gmail.com>
2023-09-22 04:41:37 +0600

    first commit

--- src/components/Supplier/Supplier.jsx
+++ src/components/Supplier/Supplier.jsx
@@ -6,13 +6,13 @@ const Supplier = () => {
       <div className="container">
         <div className="supplier-wrapper flex flex-wrap gap-x-11 px-10 lg:px-0 lg:justify-between justify-center lg:text-start text-center font-pops items-center py-36">
           <div className="left-content flex justify-end ">
-            <h2 className='font-bold w-[289px] text-5xl leading-[75px]'>The biggest
+            <h2 className='font-bold font- w-[289px] lg:text-5xl text-[40px] lg:leading-[75px]'>The biggest
               supplier on
               the country</h2>
 
           </div>
           <div className="right-content  ">
-            <p className='w-[651px]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            <p className='md:w-[651px]q'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
               The point of using Lorem Ipsum is that it has a more-or-less normal distribution.</p>
 
           </div>
