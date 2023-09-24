e6b32d8e74aa3d6593391668b2d6ba6b96dc488d
Ashraful Islam Siam <ashraful199113@gmail.com>
2023-09-24 23:12:52 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@
 
 import './App.css'
 import Header from './components/Header/Header'
-import Navbar  from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Supplier from './components/Supplier/Supplier'
 import Slider from './components/slider/slider'
 
 
 
+
 function App() {
   return (
     <>
-    <Header />
-    <Navbar />
-    <Hero/>
-    <Supplier/>
-    <Slider/>
+      <Header />
+      <Navbar />
+      <Hero />
+      <Supplier />
+      <Slider />
     </>
   )
 }
