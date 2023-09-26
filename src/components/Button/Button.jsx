cb73a4ecde3606892fb387c674289c0b2e9217d0
Ashraful Islam Siam <ashraful199113@gmail.com>
2023-09-26 13:23:57 +0600

    first commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({className,children}) => {
+const Button = ({href,className,children}) => {
   return (
-    <a className={`font-pops  hover:text-white capitalize bg-[#F40404]  text-[12px] lg:text-[16px] border font-semibold
+    <a href={`${href}`} className={`font-pops  hover:text-white capitalize bg-[#F40404]  text-[12px] lg:text-[16px] border font-semibold
     border-transparent hover:bg-transparent hover:border-white hover:scale-[1.1] duration-150 ease-in-out hover:rounded-lg w-fit ${className}`}>{children}</a>
   )
 }
